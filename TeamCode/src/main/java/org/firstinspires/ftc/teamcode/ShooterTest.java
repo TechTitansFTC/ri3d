907bd97945a044693d6b833082a1c09c0197018a
vvrsh29 <Vasudevariteshv29@gmail.com>
2025-09-19 22:05:33 -0500

    Update ShooterTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="ShooterTest", group="Test")
@@ -31,7 +32,7 @@ public class ShooterTest extends LinearOpMode {
         pusher       = hardwareMap.get(Servo.class, "pusher");
 
         // Reverse one motor so they spin opposite directions
-        shooterLeft.setDirection(DcMotor.Direction.FORWARD);
+        shooterLeft.setDirection(DcMotor.Direction.REVERSE);
         shooterRight.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();

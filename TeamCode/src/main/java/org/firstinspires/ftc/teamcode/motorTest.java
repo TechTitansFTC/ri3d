34ada69bdf4e9050feb0781a37314393bf954de4
krishvadivel <krishvadivel123@gmail.com>
2025-10-05 21:41:16 -0500

    testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest.java
@@ -11,19 +11,33 @@ public class motorTest extends LinearOpMode {
 
         DcMotor motor1 = hardwareMap.get(DcMotor.class, "motor1");
 
+        motor1.setDirection(DcMotor.Direction.FORWARD);
+        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        double power = 0;
+        boolean prevA = false;
+        boolean prevB = false;
+
+
         waitForStart();
 
         while (opModeIsActive()) {
 
-            if (gamepad1.dpad_up) {
-                motor1.setPower(1);
-            }
-            if (gamepad1.dpad_down) {
-                motor1.setPower(-1);
-            }
-            if (gamepad1.start) {
-                motor1.setPower(0);
-            }
+            if (gamepad1.dpad_up) power = 1;
+            if (gamepad1.dpad_down) power = -1;
+            if (gamepad1.start) power = 0;
+
+            // Incremental adjustment with edge detection
+            if (gamepad1.a && !prevA) power -= 0.1;
+            if (gamepad1.b && !prevB) power += 0.1;
+            prevA = gamepad1.a;
+            prevB = gamepad1.b;
+
+            // Clamp power
+            power = Math.max(-1, Math.min(1, power));
+
+            motor1.setPower(power);
+            motor1.setPower(power);
 
             telemetry.addData("Motor 1 Power: ", motor1.getPower());
             telemetry.update();
